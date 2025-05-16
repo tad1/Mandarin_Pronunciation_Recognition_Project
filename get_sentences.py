99fc8254d76a6cf8617be95ea799242843ea07ef
tad1 <tadziobrzeski@gmail.com>
2025-05-16 22:13:58 +0200

    refactor sentences

--- get_sentences.py
+++ get_sentences.py
@@ -1,11 +1,20 @@
 import polars as pl
+from tones import get_tones
 
 
-def get_info(sentence_ids):
-    """Get the info for a given sentence_id"""
-    df_csv = pl.read_csv("../../../Data/cv-corpus-20.0-2024-12-06/zh-CN/validated.tsv", separator="\t")
-    df_csv = df_csv.filter(pl.col("sentence_id").is_in(sentence_ids))
-    df_csv = df_csv.select(["sentence_id", "path", "sentence"])
-    return df_csv
 
 
+n = 10
+df_csv = pl.read_csv("../../../Data/cv-corpus-20.0-2024-12-06/zh-CN/validated.tsv", separator="\t")
+df_csv = df_csv.sample(n, with_replacement=False)
+df_csv = df_csv.select(["sentence_id", "sentence"])
+print(df_csv)
+
+for row in df_csv.iter_rows(named=True):
+    sentence_id = row["sentence_id"]
+    sentence = row["sentence"]
+    print(f"Sentence ID: {sentence_id}")
+    print(f"Sentence: {sentence}")
+    pinyin, tones = get_tones(sentence)
+    print(f"Pinyin: {pinyin}")
+    print(f"Tones: {tones}")
