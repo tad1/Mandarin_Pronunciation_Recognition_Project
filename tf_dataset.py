3d0ddce9b87d3a5b0d89dff779f0067e374e91c5
tad1 <tadziobrzeski@gmail.com>
2025-05-22 15:01:41 +0200

    add TF dataset builder

---
+++ tf_dataset.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from get_dataset import get_dataset
+
+def get_tf_dataset(size : int, useCache = True) -> tf.data.Dataset:
+    dataset_name = f"tf_{size}_dataset"
+    dataset_dir = "./data"
+    dataset_path = f"{dataset_dir}/{dataset_name}"
+    if useCache:
+        if tf.io.gfile.exists(dataset_path):
+            print(f"Loading dataset from {dataset_path}")
+            dataset = tf.data.Dataset.load(dataset_path)
+            return dataset
+    
+    print(f"Creating dataset of size {size}")
+    audio, tone_sequences = get_dataset(size)
+
+    print("converting dataset")
+    audio = [[val if val != None else 0 for val in sequence] for sequence in audio]
+    tone_sequences = [[val.value for val in sequence] for sequence in tone_sequences]
+    audio = tf.ragged.constant(audio)
+    audio = tf.expand_dims(audio, axis=-1)
+    tone_sequences = tf.ragged.constant(tone_sequences)
+
+    dataset = tf.data.Dataset.from_tensor_slices((audio, tone_sequences))
+    if useCache:
+        # tf.io.gfile.makedirs(dataset_dir)
+        dataset.save(dataset_path)
+    return dataset
+
+if __name__ == "__main__":
+    dataset = get_tf_dataset(5000, useCache=True)
+    for audio, tones in dataset.take(5):
+        print(tones)
