508415d47af48732d57af5e4c3dd3071f557c4a5
tad1 <tadziobrzeski@gmail.com>
2025-05-26 23:25:59 +0200

    something updated

--- tf_dataset.py
+++ tf_dataset.py
@@ -1,3 +1,5 @@
+from math import nan
+import math
 import tensorflow as tf
 from get_dataset import get_dataset
 
@@ -13,21 +15,25 @@ def get_tf_dataset(size : int, useCache = True) -> tf.data.Dataset:
     
     print(f"Creating dataset of size {size}")
     audio, tone_sequences = get_dataset(size)
+    max_lenght = max([len(sequence) for sequence in audio])
 
     print("converting dataset")
-    audio = [[val if val != None else 0 for val in sequence] for sequence in audio]
+    audio = [[val if not math.isnan(val) else 0 for val in sequence] for sequence in audio]
     tone_sequences = [[val.value for val in sequence] for sequence in tone_sequences]
     audio = tf.ragged.constant(audio)
+    audio = audio.to_tensor()
     audio = tf.expand_dims(audio, axis=-1)
+    
     tone_sequences = tf.ragged.constant(tone_sequences)
+    tone_sequences = tone_sequences.to_tensor()
 
     dataset = tf.data.Dataset.from_tensor_slices((audio, tone_sequences))
-    if useCache:
         # tf.io.gfile.makedirs(dataset_dir)
-        dataset.save(dataset_path)
+    dataset.save(dataset_path)
     return dataset
 
 if __name__ == "__main__":
-    dataset = get_tf_dataset(5000, useCache=True)
+    dataset = get_tf_dataset(5000, useCache=False)
     for audio, tones in dataset.take(5):
+        print(audio)
         print(tones)
