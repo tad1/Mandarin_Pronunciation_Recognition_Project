d81fdffbba5cf2ca5396d92995b24745048104e2
Kamil Fischbach <fischbach.kamil@gmail.com>
2025-06-24 12:27:03 +0200

    playing with W2V without success

--- kamils_exploration/getStageIDataset.py
+++ kamils_exploration/getStageIDataset.py
@@ -7,7 +7,18 @@ stageIpath = "../../../recordings/stageI"
 valid_indices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100]
 
 
-def getStageIDataset():
+def getStageIDataset(addTones=False, onlyPolish=True):
+
+    # Read the demographics data and keep only Polish native speakers
+    demographics = pd.read_csv("../../../experiment.csv").drop(columns="agent")
+    demographics = demographics[
+        ~(demographics["recordingsI"].isnull() & demographics["recordingsII"].isnull())
+    ]
+    if onlyPolish:
+        demographics = demographics[
+            demographics["mother"].str.contains("olish|olski", case=False, na=False)
+        ]
+
     # Read the assesment data,
     # remove the id_evaluator column and any rows with id_student null
     assesmentDf = pd.read_csv("../../../assesment.csv")
@@ -19,6 +30,11 @@ def getStageIDataset():
     assesmentCleanDf = assesmentCleanDf.drop(
         columns=[col for col in assesmentCleanDf.columns if col.startswith("q")]
     )
+    # Keeping only the assesments of Polish native speakers
+    assesmentCleanDf = assesmentCleanDf[
+        assesmentCleanDf["id_student"].isin(demographics["id"])
+    ]
+
     # oggToWav()  # Transform all stageI ogg to wav, as torchaudio had issues loading the ogg
     # Store the file paths if aXp is not NaN
     rows = []
@@ -34,13 +50,16 @@ def getStageIDataset():
                 tone = row[t_col] if t_col in row else np.nan
                 file_path = f"{stageIpath}/{student_id}/a{i}.wav"
                 if os.path.exists(file_path):
-                    rows.append(
-                        {
-                            "filepath": file_path,
-                            "pron": int(pron),
-                            "tone": int(tone) if not pd.isna(tone) else np.nan,
-                        }
-                    )
+                    if not addTones:
+                        rows.append({"filepath": file_path, "pron": int(pron)})
+                    else:
+                        rows.append(
+                            {
+                                "filepath": file_path,
+                                "pron": int(pron),
+                                "tone": int(tone) if not pd.isna(tone) else np.nan,
+                            }
+                        )
 
     final_df = pd.DataFrame(rows)
     return final_df
